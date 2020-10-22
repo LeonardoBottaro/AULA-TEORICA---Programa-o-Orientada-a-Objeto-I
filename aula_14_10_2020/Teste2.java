4db538e81b1fe9bd6072d5dbd39547976218da6e
leonardo.bottaro <leonardooloiro@hotmail.com>
2020-10-21 21:37:29 -0300

    Aula 14/10 completa

---
+++ aula_14_10_2020/Teste2.java
@@ -0,0 +1,13 @@
+package aula_14_10_2020;
+
+public class Teste2 {
+    
+    public static void main(String[] args) {
+        
+            Cliente c = null;
+            c = Cliente.novoCliente();
+            c.getCodigo();
+            System.out.println(c);
+    }
+
+}
