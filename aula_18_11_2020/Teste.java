55d45879423d01c77cffba59d3ac698e36af03a6
leonardo.bottaro <leonardooloiro@hotmail.com>
2020-11-22 16:16:12 -0300

    Manipulação de Data

---
+++ aula_18_11_2020/Teste.java
@@ -0,0 +1,27 @@
+package aula_18_11_2020;
+
+
+import java.time.LocalDateTime;
+import java.time.DayOfWeek;
+
+public class Teste {
+    public static void main(String[] args) {
+
+            LocalDateTime inicio = LocalDateTime.now();
+
+            LocalDateTime fim = inicio.plusDays(4);
+
+            System.out.println(inicio);
+            System.out.println(fim);
+
+            if(fim.getDayOfWeek().equals(DayOfWeek.SUNDAY))
+                 System.out.println("Domingo");
+
+
+           if(fim.isAfter(inicio))
+           {
+               System.out.println("Data de fim maior que a data de início");
+               System.out.println("Dias corridos: " + fim.compareTo(inicio));
+           }
+    }
+}
